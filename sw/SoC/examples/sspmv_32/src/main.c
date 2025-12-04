fc91098fabfd076b71bf455c36ab013d2bab46a6
annalia01 <annalia.ruggiero01@gmail.com>
2025-12-04 09:27:08 +0100

    Create main.c

---
+++ sw/SoC/examples/sspmv_32/src/main.c
@@ -0,0 +1,98 @@
+// Copyright 2022 ETH Zurich and University of Bologna.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Author: Chi Zhang, ETH Zurich <chizhang@iis.ee.ethz.ch>
+
+#include <stdint.h>
+#include <string.h>
+
+#include "sspmv.h"
+#include "runtime.h"
+#include "util.h"
+
+#define MSTATUS_FS (0x000006000U)
+#define MSTATUS_XS (0x000018000U)
+#define MSTATUS_VS (0x000000600U)
+#define NR_LANES 2
+#include "uninasoc.h"
+
+
+extern int32_t cols;
+extern int32_t rows;
+extern int32_t M;
+extern int32_t N;
+
+extern float VALUES[] __attribute__((aligned(32 * NR_LANES)));
+extern int32_t col_idx[] __attribute__((aligned(32 * NR_LANES)));
+extern float IN_VEC[] __attribute__((aligned(32 * NR_LANES)));
+extern float OUT_VEC[] __attribute__((aligned(32 * NR_LANES)));
+
+static inline uint64_t read_minstret(void) {
+    uint64_t value;
+    asm volatile ("csrr %0, instret" : "=r"(value));
+    return value;
+}
+
+int main() {
+  uninasoc_init();
+  printf("\n");
+  printf("==========\n");
+  printf("=  SpMV  =\n");
+  printf("==========\n");
+  printf("\n");
+  printf("\n");
+  asm volatile (" li      t0, %0     " :: "i"(MSTATUS_FS | MSTATUS_XS | MSTATUS_VS));
+  asm volatile (" csrs    mstatus, t0" );
+
+  printf("\n");
+  printf(
+      "-------------------------------------------------------------------\n");
+  printf(
+      "Calculating a (%d x %d) x %d sparse matrix vector multiplication...\n",
+      rows, cols);
+ 
+  printf(
+      "-------------------------------------------------------------------\n");
+  printf("\n");
+
+  printf("calculating ... \n");
+  #ifdef SPIKEGEM
+  uint64_t start_minstret = read_minstret();
+  #endif 
+  start_timer();
+  sspmv_32(M, N, rows, cols, VALUES, col_idx, IN_VEC, OUT_VEC);
+  stop_timer();
+  #ifdef SPIKEGEM
+  uint64_t end_minstret = read_minstret();
+  uint64_t delta_minstret = end_minstret - start_minstret;
+  #endif
+  // Metrics
+  int64_t runtime = get_timer();
+    
+  #ifdef SPIKEGEM
+  printf("Instructions retired (CSR minstret): %lu\n", delta_minstret);
+  #endif
+  printf("The execution took %d cycles.\n", runtime);
+
+
+  printf("Verifying ...\n");
+  if (sspmv_verify(M, N, rows, cols, VALUES, col_idx, IN_VEC, OUT_VEC)) {
+    return 1;
+  } else {
+    printf("Passed.\n");
+  }
+  return 0;
+}
